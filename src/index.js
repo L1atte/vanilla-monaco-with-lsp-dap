68b838f3f63681c90e396be6d4debf61fb8f207b
Latte <l1atte@foxmail.com>
2023-03-03 18:32:25 +0800

    feat: change language

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const MODEL_URI = "inmemory://model.js";
 const MONACO_URI = monaco.Uri.parse(MODEL_URI);
 const VALUE = `const a = 'hello world'
 console.log(a)`;
+let isJavaScript = true;
 
 // register Monaco languages
 monaco.languages.register({
@@ -15,7 +16,7 @@ monaco.languages.register({
 });
 
 // create monaco instance
-monaco.editor.create(document.getElementById("container"), {
+const instance = monaco.editor.create(document.getElementById("container"), {
 	// create monaco model
 	model: monaco.editor.createModel(VALUE, LANGUAGE_ID, MONACO_URI),
 	glyphMargin: true,
@@ -23,4 +24,48 @@ monaco.editor.create(document.getElementById("container"), {
 		enabled: true,
 	},
 	automaticLayout: true,
+});
+
+// 语言切换
+const switchBtnRef = document.getElementById("switchLang");
+switchBtnRef.addEventListener("click", () => {
+	instance.dispose();
+	isJavaScript = !isJavaScript;
+	if (isJavaScript) {
+		// register Monaco languages
+		monaco.languages.register({
+			id: LANGUAGE_ID,
+			extensions: [".js", ".javascript"],
+			aliases: ["JS", "js", "javascript", "JavaScript"],
+		});
+
+		// create monaco instance
+		monaco.editor.create(document.getElementById("container"), {
+			// create monaco model
+			model: monaco.editor.createModel(VALUE, LANGUAGE_ID, MONACO_URI),
+			glyphMargin: true,
+			lightbulb: {
+				enabled: true,
+			},
+			automaticLayout: true,
+		});
+	} else {
+		// register Monaco languages
+		monaco.languages.register({
+			id: "html",
+			extensions: [".html"],
+			aliases: ["html"],
+		});
+
+		// create monaco instance
+		monaco.editor.create(document.getElementById("container"), {
+			// create monaco model
+			model: monaco.editor.createModel("", "html", monaco.Uri.parse("inmemory://model.html")),
+			glyphMargin: true,
+			lightbulb: {
+				enabled: true,
+			},
+			automaticLayout: true,
+		});
+	}
 });
