c19670bb0a79604399d7e2e3c5c3c792a7fdbf21
Latte <l1atte@foxmail.com>
2023-03-09 18:43:39 +0800

    feat: add antlr-c3

--- src/example/auto.js
+++ src/example/auto.js
@@ -1,10 +1,18 @@
-import { autosuggester } from "antlr4-autosuggest";
 import JavaScriptLexer from "../ANTLR2/JavaScriptLexer.js";
 import JavaScriptParser from "../ANTLR2/JavaScriptParser.js";
+import { ANTLRErrorListener, CharStreams, CommonToken, CommonTokenStream, RecognitionException, Recognizer, Token } from "antlr4ts";
+import * as CodeCompletionCore from "antlr4-c3/out/src/CodeCompletionCore.js";
 
-const auto = autosuggester(JavaScriptLexer, JavaScriptParser);
-console.log(auto);
+const inputStream = CharStreams.fromString("var c = a + b()");
+const lexer = new JavaScriptLexer(inputStream);
+const tokenStream = new CommonTokenStream(lexer);
 
-debugger
-const suggestions = auto.autosuggest("cons");
-console.log(suggestions);
+const parser = new JavaScriptParser(tokenStream);
+// const errorListener = new ErrorListener();
+// parser.addErrorListener(errorListener);
+parser.expression();
+
+const core = new CodeCompletionCore(parser);
+// 1) At the input start.
+let candidates = core.collectCandidates(0);
+console.log(core, candidates);
